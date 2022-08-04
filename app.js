4f5ee03fb56fdd97c4786d93fb97fe5a340d58c9
Rahul Ranjan <rrahulranjan26@gmail.com>
2022-08-04 16:29:58 +0530

    Swagger UI Added

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
 
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
 app.use(rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 Minutes
     max: 100 // Limit each IP to 100 requests per window
@@ -36,22 +41,19 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-//Testing route
-// app.use('/', (req, res) => {
-//     res.send("In Jobs API ")
-// })
 
 
 // routes
 
+//Test Api Router
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', auth, jobRouter);
-
-
-//Test Api Router
-app.use('/', (req, res) => {
-    res.send("Successful!!")
-})
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
